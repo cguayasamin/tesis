90ff088be4a47fff9d9f55e46092ca779a8f2984
cinty1509Gll <53570848+cinty1509Gll@users.noreply.github.com>
2021-03-01 08:14:51 -0500

    first commit

---
+++ doctoradoEjb/ejbModule/ec/edu/ec/doctorado/ejb/entidad/PersonaServicio.java
@@ -0,0 +1,10 @@
+package ec.edu.ec.doctorado.ejb.entidad;
+
+import java.util.List;
+
+import ec.edu.uce.doctorado.jpa.entidad.Persona;
+
+public interface PersonaServicio {
+
+	public List<Persona> listar();
+}
---
+++ doctoradoJsf/src/ec/edu/uce/doctorado/jsf/entidad/PersonaControlador.java
@@ -0,0 +1,26 @@
+package ec.edu.uce.doctorado.jsf.entidad;
+
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+
+import ec.edu.ec.doctorado.ejb.entidad.PersonaServicio;
+import ec.edu.uce.doctorado.jpa.entidad.Persona;
+
+
+
+@ManagedBean
+@RequestScoped
+public class PersonaControlador {
+	
+	//@EJB
+	private PersonaServicio personaServicio;
+	
+	private List<Persona> listarP;
+	
+	public void listarT() {
+		listarP=personaServicio.listar();
+	}
+}
